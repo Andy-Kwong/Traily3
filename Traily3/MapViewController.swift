0f2bfa4115f3c63c019133c58ea41df1c5a687e1
Omar Ihmoda <omarihmoda@Omars-MacBook-Pro.local>
2018-01-18 22:09:37 -0800

    added trail GeoJSON and Core Data

--- Traily3/MapViewController.swift
+++ Traily3/MapViewController.swift
@@ -8,19 +8,199 @@
 
 import UIKit
 import Mapbox
+import CoreLocation
 
-class MapViewController: UIViewController {
-
+class MapViewController: UIViewController, MGLMapViewDelegate, CLLocationManagerDelegate {
+    var lat: Double = 0.0
+    var long: Double = 0.0
+    
+    let locationManager = CLLocationManager()
+    
+    var mapView: MGLMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        enableBasicLocationServices()
+        
+        mapView = MGLMapView(frame: view.bounds)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        mapView.setCenter(CLLocationCoordinate2D(latitude: 45.5076, longitude: -122.6736),
+                          zoomLevel: 11, animated: false)
+        view.addSubview(self.mapView)
+        
+        mapView.delegate = self
+        
+        mapView.showsUserLocation = true
+        
+        drawPolyline()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func drawPolyline() {
+        // Parsing GeoJSON can be CPU intensive, do it on a background thread
+        
+        DispatchQueue.global(qos: .background).async(execute: {
+            // Get the path for example.geojson in the app's bundle
+            let jsonPath = Bundle.main.path(forResource: "tracks2", ofType: "geojson")
+            let url = URL(fileURLWithPath: jsonPath!)
+            
+            do {
+                // Convert the file contents to a shape collection feature object
+                let data = try Data(contentsOf: url)
+                let shapeCollectionFeature = try MGLShape(data: data, encoding: String.Encoding.utf8.rawValue) as! MGLShapeCollectionFeature
+                
+                if let polyline = shapeCollectionFeature.shapes.first as? MGLPolylineFeature {
+                    // Optionally set the title of the polyline, which can be used for:
+                    //  - Callout view
+                    //  - Object identification
+                    polyline.title = polyline.attributes["name"] as? String
+                    
+                    // Add the annotation on the main thread
+                    DispatchQueue.main.async(execute: {
+                        // Unowned reference to self to prevent retain cycle
+                        [unowned self] in
+                        self.mapView.addAnnotation(polyline)
+                    })
+                }
+            }
+            catch {
+                print("GeoJSON parsing failed")
+            }
+            
+        })
+        
     }
-
-
+    
+    func drawWayPoints(){
+        // Parsing GeoJSON can be CPU intensive, do it on a background thread
+        DispatchQueue.global(qos: .background).async(execute: {
+            // Get the path for example.geojson in the app's bundle
+            let jsonPath = Bundle.main.path(forResource: "tracks2", ofType: "geojson")
+            let url = URL(fileURLWithPath: jsonPath!)
+            
+            do {
+                // Convert the file contents to a shape collection feature object
+                let data = try Data(contentsOf: url)
+                let shapeCollectionFeature = try MGLShape(data: data, encoding: String.Encoding.utf8.rawValue) as! MGLShapeCollectionFeature
+                
+                if let polyline = shapeCollectionFeature.shapes.first as? MGLPolylineFeature {
+                    // Optionally set the title of the polyline, which can be used for:
+                    //  - Callout view
+                    //  - Object identification
+                    polyline.title = polyline.attributes["name"] as? String
+                    
+                    // Add the annotation on the main thread
+                    DispatchQueue.main.async(execute: {
+                        // Unowned reference to self to prevent retain cycle
+                        [unowned self] in
+                        self.mapView.addAnnotation(polyline)
+                    })
+                }
+            }
+            catch {
+                print("GeoJSON parsing failed")
+            }
+            
+        })
+        
+    }
+    
+    func mapView(_ mapView: MGLMapView, alphaForShapeAnnotation annotation: MGLShape) -> CGFloat {
+        // Set the alpha for all shape annotations to 1 (full opacity)
+        return 1
+    }
+    
+    func mapView(_ mapView: MGLMapView, lineWidthForPolylineAnnotation annotation: MGLPolyline) -> CGFloat {
+        // Set the line width for polyline annotations
+        return 2.0
+    }
+    
+    func mapView(_ mapView: MGLMapView, strokeColorForShapeAnnotation annotation: MGLShape) -> UIColor {
+        // Give our polyline a unique color by checking for its `title` property
+        if (annotation.title == "Crema to Council Crest" && annotation is MGLPolyline) {
+            // Mapbox cyan
+            return UIColor(red: 59/255, green:178/255, blue:208/255, alpha:1)
+        }
+        else
+        {
+            return .red
+        }
+    }
+    
+    func enableBasicLocationServices() {
+        locationManager.delegate = self
+        
+        switch CLLocationManager.authorizationStatus() {
+        case .notDetermined:
+            // Request when-in-use authorization initially
+            locationManager.requestWhenInUseAuthorization()
+            break
+            
+        case .restricted, .denied:
+            // Disable location features
+            disableMyLocationBasedFeatures()
+            break
+            
+        case .authorizedWhenInUse, .authorizedAlways:
+            // Enable location features
+            enableMyWhenInUseFeatures()
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .restricted, .denied:
+            disableMyLocationBasedFeatures()
+            break
+            
+        case .authorizedWhenInUse:
+            enableMyWhenInUseFeatures()
+            break
+            
+        case .notDetermined, .authorizedAlways:
+            break
+        }
+    }
+    
+    func disableMyLocationBasedFeatures(){
+        print("disableMyLocationBasedFeatures")
+    }
+    
+    func enableMyWhenInUseFeatures(){
+        print("enableMyWhenInUseFeaatures")
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.distanceFilter = 1.0 // In meters.
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager,  didUpdateLocations locations: [CLLocation]) {
+        let lastLocation = locations.last!
+        if self.lat == 0.0 {
+            self.lat = lastLocation.coordinate.latitude
+            self.long = lastLocation.coordinate.longitude
+            mapView.setCenter(CLLocationCoordinate2D(latitude: self.lat, longitude: self.long),
+                              zoomLevel: 11, animated: false)
+        } else {
+            calcDistance(latitude1: self.lat, longitude1: self.long, latitude2: lastLocation.coordinate.latitude , longitude2: lastLocation.coordinate.longitude)
+        }
+        
+        // Do something with the location.
+    }
+    
+    func calcDistance (latitude1: Double, longitude1: Double, latitude2: Double, longitude2: Double) -> Double {
+        
+        let coordinate1 = CLLocation(latitude: latitude1, longitude: longitude2)
+        let coordinate2 = CLLocation(latitude: latitude2, longitude: longitude2)
+        
+        let distanceInMeters = coordinate1.distance(from: coordinate2) // result is in meters
+        print(distanceInMeters)
+        return distanceInMeters
+    }
+    
+    
+    
 }
 
