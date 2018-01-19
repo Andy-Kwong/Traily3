7454c2206fcb97e29596c270db857f13030bcefd
JeffBaucom <jeffbaucom@gmail.com>
2018-01-18 22:28:14 -0800

    add function to pop up alert, add functions to display annotations w/ or w/o images

--- Traily3/MapViewController.swift
+++ Traily3/MapViewController.swift
@@ -9,11 +9,20 @@
 import UIKit
 import Mapbox
 
-class MapViewController: UIViewController {
-
+class MapViewController: UIViewController, MGLMapViewDelegate {
+    @IBOutlet weak var mapView: MGLMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.delegate = self
+        let annotation = MyCustomPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 37.3753997, longitude: -121.9101584)
+        annotation.title = "Coding Dojo"
+        annotation.subtitle = "The Dojo, Yo"
+        annotation.willUseImage = true
+        mapView.addAnnotation(annotation)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +30,70 @@ class MapViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func popUp() {
+        let alert = UIAlertController(title: "My Alert", message: "This is an alert.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .`default`, handler: { _ in
+            NSLog("The \"OK\" alert occured.")
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    // This delegate method is where you tell the map to load a view for a specific annotation based on the willUseImage property of the custom subclass.
+    func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? {
+        
+        if let castAnnotation = annotation as? MyCustomPointAnnotation {
+            if (castAnnotation.willUseImage) {
+                return nil;
+            }
+        }
+        
+        // Assign a reuse identifier to be used by both of the annotation views, taking advantage of their similarities.
+        let reuseIdentifier = "reusableDotView"
+        
+        // For better performance, always try to reuse existing annotations.
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier)
+        
+        // If there’s no reusable annotation view available, initialize a new one.
+        if annotationView == nil {
+            annotationView = MGLAnnotationView(reuseIdentifier: reuseIdentifier)
+            annotationView?.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+            annotationView?.layer.cornerRadius = (annotationView?.frame.size.width)! / 2
+            annotationView?.layer.borderWidth = 4.0
+            annotationView?.layer.borderColor = UIColor.white.cgColor
+            annotationView!.backgroundColor = UIColor(red:0.03, green:0.80, blue:0.69, alpha:1.0)
+        }
+        
+        return annotationView
+    }
+    
+    // This delegate method is where you tell the map to load an image for a specific annotation based on the willUseImage property of the custom subclass.
+    func mapView(_ mapView: MGLMapView, imageFor annotation: MGLAnnotation) -> MGLAnnotationImage? {
+        
+        if let castAnnotation = annotation as? MyCustomPointAnnotation {
+            if (!castAnnotation.willUseImage) {
+                return nil;
+            }
+        }
+        
+        // For better performance, always try to reuse existing annotations.
+        var annotationImage = mapView.dequeueReusableAnnotationImage(withIdentifier: "camera")
+        
+        // If there is no reusable annotation image available, initialize a new one.
+        if(annotationImage == nil) {
+            annotationImage = MGLAnnotationImage(image: UIImage(named: "camera")!, reuseIdentifier: "camera")
+        }
+        
+        return annotationImage
+    }
+    
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        // Always allow callouts to popup when annotations are tapped.
+        return true
+    }
 }
+
+class MyCustomPointAnnotation: MGLPointAnnotation {
+    var willUseImage: Bool = false
+}
+
 
