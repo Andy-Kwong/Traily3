5155a2a42a4cb6c3d451024e9b9a05c0a574d73b
Andy Kwong <andy.kwong@me.com>
2018-01-18 20:46:53 -0800

    added assets and mapbox

---
+++ Traily3/MapViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Traily3
+//
+//  Created by Andy Kwong on 1/18/18.
+//  Copyright © 2018 Andy Kwong. All rights reserved.
+//
+
+import UIKit
+import Mapbox
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
